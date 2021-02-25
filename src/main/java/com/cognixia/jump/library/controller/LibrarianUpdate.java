ef9552240834bead25c59dd88c0c7c91edf1d29e
dpvpp <dpvpp@Swagtop>
2021-02-25 15:19:48 -0500

    Librarian update servlet

--- src/main/java/com/cognixia/jump/library/controller/LibrarianUpdate.java
+++ src/main/java/com/cognixia/jump/library/controller/LibrarianUpdate.java
@@ -6,22 +6,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.cognixia.jump.library.dao.LibrarianDAO;
+import com.cognixia.jump.library.dao.LibrarianDAOClass;
+import com.cognixia.jump.library.model.Librarian;
+
+
 
 public class LibrarianUpdate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
     
+	private LibrarianDAO db;
+	
     public LibrarianUpdate() {
         super();
-        // TODO Auto-generated constructor stub
     }
     
+    @Override
+    public void init() throws ServletException {
+    	this.db = new LibrarianDAOClass();
+    }
     
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		//ID must be passed to this servlet
+		int id = Integer.parseInt(request.getParameter("id"));
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
 		
-		//yet to update my code here to do a patch request
+		db.updateLibrarian(new Librarian(id,username,password)); 
+		
+		javax.servlet.RequestDispatcher dispatcher = request.getRequestDispatcher("/");
+		dispatcher.forward(request, response);
+		
 	}
 
 }
