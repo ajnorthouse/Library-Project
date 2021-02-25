4dad95c1624ef1a4c98e9bfd1d7f8ecb5ff04700
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-02-25 15:44:37 -0500

    Mostly complete librarian login implementation.

--- src/main/java/com/cognixia/jump/library/controller/LibrarianLogin.java
+++ src/main/java/com/cognixia/jump/library/controller/LibrarianLogin.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import com.cognixia.jump.library.dao.LibrarianDAO;
 import com.cognixia.jump.library.dao.LibrarianDAOClass;
+import com.cognixia.jump.library.helper.LoginHelper;
+import com.cognixia.jump.library.model.Librarian;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
@@ -13,6 +15,8 @@ import jakarta.servlet.http.HttpServletResponse;
 public class LibrarianLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private LibrarianDAO db;
+	private Librarian librarian;
+	private int result;
 	
 
     @Override
@@ -26,16 +30,39 @@ public class LibrarianLogin extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//collect info from jsp
-		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
 		
 		//create temporary object.
-		
+		if (librarian == null) {
+			librarian = new Librarian();
+		}
+		librarian.setUsername(username);
+		librarian.setPassword(password);
 		
 		//send to class that helps check login info.
-		
+		result = LoginHelper.checkLogin(librarian, db);
 		
 		//update jsp with feedback OR send to new page.
+		switch (result){
 		
+		//bad password
+		case -1:
+			//code to update jsp
+			request.setAttribute("error", "Bad password match, please try again.");
+			break;
+			
+		//bad username
+		case 0:
+			//code to update jsp
+			request.setAttribute("error", "No matching username in database, please try again.");
+			break;
+		
+		//successful login
+		default:
+			request.setAttribute("librarian_id", result);
+			break;
+		}
 	}
-
+	
 }
