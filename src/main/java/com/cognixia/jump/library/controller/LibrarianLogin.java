d4449f8e2b29324415718c73ff25c530581344ac
Alexandre Northouse <Alex.J.Northouse@gmail.com>
2021-02-26 10:48:31 -0500

    Remove unused imports

--- src/main/java/com/cognixia/jump/library/controller/LibrarianLogin.java
+++ src/main/java/com/cognixia/jump/library/controller/LibrarianLogin.java
@@ -1,15 +1,11 @@
 package com.cognixia.jump.library.controller;
 
 import java.io.IOException;
-import java.util.List;
-
 
 import com.cognixia.jump.library.dao.LibrarianDAO;
 import com.cognixia.jump.library.dao.LibrarianDAOClass;
-import com.cognixia.jump.library.dao.PatronDAOClass;
 import com.cognixia.jump.library.helper.LoginHelper;
 import com.cognixia.jump.library.model.Librarian;
-import com.cognixia.jump.library.model.Patron;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
