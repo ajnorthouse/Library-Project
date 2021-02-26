b104c1db2ba1bc06c07e9b695323bbb1b9fceee3
dpvpp <dpvpp@Swagtop>
2021-02-26 12:33:08 -0500

    changed javax to jakarta

--- src/main/java/com/cognixia/jump/library/controller/BookServlet.java
+++ src/main/java/com/cognixia/jump/library/controller/BookServlet.java
@@ -4,11 +4,11 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.cognixia.jump.library.dao.BookDAO;
 import com.cognixia.jump.library.dao.BookDAOClass;
