106d9bdd4714365833d426373ce20ac551e22c81
rhlaroya <rhlaroya@gmail.com>
2021-02-26 10:20:48 -0600

    Added class variables to the PatronUpdate

--- src/main/java/com/cognixia/jump/library/controller/PatronUpdate.java
+++ src/main/java/com/cognixia/jump/library/controller/PatronUpdate.java
@@ -1,38 +1,45 @@
 package com.cognixia.jump.library.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
+import com.cognixia.jump.library.dao.PatronDAO;
+import com.cognixia.jump.library.dao.PatronDAOClass;
+import com.cognixia.jump.library.model.Patron;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class PatronUpdate
  */
 public class PatronUpdate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private PatronDAO db;
+	private Patron patron;
+	
+	@Override
+	public void init() throws ServletException {
+		db = new PatronDAOClass();
+	}
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public PatronUpdate() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//Pull database 
+		//Redirect to the update page
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+		//collect update data
+		//pull data base
 		doGet(request, response);
 	}
 
--- src/main/webapp/update_patron.jsp
+++ src/main/webapp/update_patron.jsp
@@ -5,12 +5,20 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Update Profile</title>
+<jsp:useBean id = "patronbean" class = "Patron" scope = "session" />
+<jsp:useBean id="patron" class = "Patron" scope="page"></jsp:useBean>
+<%Integer patron_id = new Integer(request.getParameter("patron_id"));
+  patron = patronbean.getPatronById(patron_id.intValue());%>
 </head>
 <body>
   <h1>Update Profile</h1>
   <h2>Welcome <%=request.getAttribute("first_name" + "last_name") %></h2>
 <form action="" method="post"></form>
 	<table style="width: 50%">
+	  <tr>
+	    <td><input type="hidden" id="patron_id" name="patron_id" 
+	    value="<%= patron.getPatronId()%>"></td>
+	  </tr>
 	  <tr>
 		<td>First Name</td>
 		<td><input type="text" name="first_name" /></td>
