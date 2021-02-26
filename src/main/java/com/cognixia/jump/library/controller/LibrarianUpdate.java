e926aebf354a20cad47c319f41ead2a274407408
dpvpp <dpvpp@Swagtop>
2021-02-26 14:49:18 -0500

    Librarian update

--- src/main/java/com/cognixia/jump/library/controller/LibrarianUpdate.java
+++ src/main/java/com/cognixia/jump/library/controller/LibrarianUpdate.java
@@ -1,20 +1,24 @@
 package com.cognixia.jump.library.controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import com.cognixia.jump.library.dao.LibrarianDAO;
 import com.cognixia.jump.library.dao.LibrarianDAOClass;
 import com.cognixia.jump.library.model.Librarian;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpSession;
 
 
 
 public class LibrarianUpdate extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
+	HttpSession session;
 	private LibrarianDAO db;
 	
     public LibrarianUpdate() {
@@ -26,18 +30,37 @@ public class LibrarianUpdate extends HttpServlet {
     	this.db = new LibrarianDAOClass();
     }
     
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		session = request.getSession();
+		
+		if(session.getAttribute("librarian_id")!=null){
+		    int id = ((int)session.getAttribute("librarian_id"));
+		    Librarian librarian = db.getLibrarianByID(id);
+		    request.setAttribute("username", librarian.getUsername());
+		    request.setAttribute("password", librarian.getPassword());
+		    RequestDispatcher dispatcher = request.getRequestDispatcher("/update_librarian.jsp");
+		    dispatcher.forward(request, response);
+		}
+		else{    
+			System.out.println("No ID");
+		}
+	}
+
+    
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//ID must be passed to this servlet
-		int id = Integer.parseInt(request.getParameter("id"));
+		int id = (int) session.getAttribute("librarian_id");
 		String username = request.getParameter("username");
 		String password = request.getParameter("password");
+
+		session.setAttribute("username", username);
 		
 		db.updateLibrarian(new Librarian(id,username,password)); 
 		
-		javax.servlet.RequestDispatcher dispatcher = request.getRequestDispatcher("/");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/librarian.jsp");
 		dispatcher.forward(request, response);
 		
 		
 	}
-
 }
